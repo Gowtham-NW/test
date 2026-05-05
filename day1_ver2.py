7b22e0ae2d6a3105d44e8163a3ef77bf8606d94f
Gowtham-NW <gowtham@neurowhale.ai>
2026-05-05 17:56:41 +0530

    updated ver2

---
+++ day1_ver2.py
@@ -0,0 +1,62 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from transformers import pipeline
+
+# Loading document, using langchain community document loaders
+loader = PyPDFLoader("Data Scientist.pdf")
+documents = loader.load()
+
+# Split text, using langchain_text_splitters
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+docs = text_splitter.split_documents(documents)
+
+# Embeddings
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+
+# 5. Vector DB storage by langchain vectorstores, using FAISS for smart search
+vectorstore = FAISS.from_documents(docs, embeddings)
+
+
+# 6. Retriever
+retriever = vectorstore.as_retriever()
+
+# 7. LLM (Better model)
+pipe = pipeline(
+    "text2text-generation",
+    model="google/flan-t5-base",
+    max_new_tokens=120,
+    do_sample=False
+)
+
+# user Query
+query = "what is this pdf about"
+
+# Retrieve relevant docs
+retrieved_docs = retriever.invoke(query)
+
+# Combine context
+context = "\n".join([doc.page_content for doc in retrieved_docs])
+
+# prompt engineering
+prompt = f"""
+Answer the question clearly in 1-2 sentences.
+
+Context:
+{context}
+
+Question: {query}
+
+Answer:
+"""
+
+# Generate response (direct pipeline)
+result = pipe(prompt)
+
+# Extract answer
+answer = result[0]["generated_text"]
+
+print("\n✅ Final Answer:\n")
+print(answer.strip())
